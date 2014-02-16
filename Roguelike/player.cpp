eac767498d3eedd54d5c02640bd3e0657fef6d1a
threedliams <threedliams@gmail.com>
2014-02-15 23:31:13 -0500

    added player files and the outline of a player class

---
+++ Roguelike/player.cpp
@@ -0,0 +1,17 @@
+
+
+
+
+#include "player.h"
+
+
+using namespace std;
+
+
+
+void Player::levelUp(){
+	experience = 0;
+	level++;
+
+	//increase stats by some sort of modifiers here. possibly dependent on race or something
+}
---
+++ Roguelike/player.h
@@ -0,0 +1,27 @@
+
+
+
+
+class Player{
+	int level;
+	int experience;
+
+	int health;
+	int hunger;
+
+	int speed;
+	int armor;
+
+	int intelligence;
+	int dexterity;
+	int wisdom;
+	int charisma;
+	int agility;
+	int strength;
+	int luck;
+
+	void levelUp();
+
+public:
+
+};
